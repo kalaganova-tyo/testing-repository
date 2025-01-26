a37745b891863ac352cfa3c93479de2e0b98db75
Kalaganova-Tyo <tiwared@yandex.ru>
2025-01-26 17:47:22 +0300

    Add calcul.bat and auto_add_path.py for PATH

---
+++ auto_add_to_path.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+target = os.path.join(os.path.expanduser('~'))
+os.makedirs(target, exist_ok=True)
+name = os.path.join()
---
+++ calcul.bat
@@ -0,0 +1,2 @@
+@echo off
+python "C:\Users\user\PycharmProjects\testing-repository\subparser.py" %*
